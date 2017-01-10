34dbec24a0f5184dbb1f510a832655275c422fe3
Qingqian <zhaoqingqian21@hotmail.com>
2017-01-10 16:32:25 -0600

    Add files via upload
    Linked list related practice problems

---
+++ src/LinkedList/DeleteNodeWithGreaterValueOnRightSide.java
@@ -0,0 +1,48 @@
+package LinkedList;
+
+import java.util.ArrayList;
+
+public class DeleteNodeWithGreaterValueOnRightSide {
+	public ListNode deleteNodes(ListNode head) {
+		ArrayList<Integer> max = new ArrayList<Integer>();
+		max.add(Integer.MIN_VALUE);
+		return deleteNodes(head, max);
+	}
+	public ListNode deleteNodes(ListNode head, ArrayList<Integer> max) {
+		if(head == null)
+			return null;
+		ListNode next = deleteNodes(head.next, max);
+		if(head.val > max.get(0)) {
+			max.add(0,head.val);
+		} else {
+			return next;
+		}
+		head.next = next;
+		return head;
+	}
+	
+	public static void print(ListNode head) {
+		ListNode current = head;
+		while(current != null) {
+			System.out.print(current.val + " ");
+			current = current.next;
+		}
+	}
+	
+	public static void main(String[] args) {
+		//12->15->10->11->5->6->2->3
+		ListNode head = new ListNode(12);
+		head.next = new ListNode(15);
+		head.next.next = new ListNode(10);
+		head.next.next.next = new ListNode(11);
+		head.next.next.next.next = new ListNode(5);
+		head.next.next.next.next.next = new ListNode(6);
+		head.next.next.next.next.next.next = new ListNode(2);
+		head.next.next.next.next.next.next.next = new ListNode(3);
+		
+		DeleteNodeWithGreaterValueOnRightSide object = new DeleteNodeWithGreaterValueOnRightSide();
+		head = object.deleteNodes(head);
+		print(head);
+		
+	}
+}
---
+++ src/LinkedList/RemoveMiddleElementInSegementedLinkedList.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+/*	http://www.geeksforgeeks.org/given-linked-list-line-segments-remove-middle-points
+ *  interesting problem
+ *  We should keep track the current, next and next.next node 
+ * */
+
+public class RemoveMiddleElementInSegementedLinkedList {
+
+}
