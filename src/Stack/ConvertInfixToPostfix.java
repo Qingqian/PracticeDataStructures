78a231a4e29981012437b184856e7c2a93a3633e
Qingqian Vivian Zhao <zhaoqingqian21@hotmail.com>
2017-01-15 19:17:06 -0600

    Add files via upload

--- src/Stack/ConvertInfixToPostfix.java
+++ src/Stack/ConvertInfixToPostfix.java
@@ -1,7 +1,12 @@
 package Stack;
 
 import java.util.*;
-
+/*
+ * Question: http://quiz.geeksforgeeks.org/stack-set-2-infix-to-postfix/
+ * Author : vzhao
+ * Noted : The following solution will only work if the representation of a number is a letter. 
+ * 		   Otherwise, you should use array or a list to represent the number
+ */
 public class ConvertInfixToPostfix {
 	public String InfixToPostfix(String input) {
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
@@ -46,4 +51,60 @@ public class ConvertInfixToPostfix {
 		String postfix = convert.InfixToPostfix(input);
 		System.out.println(postfix);
 	}
+	
+	public static int postfixEvaluation(ArrayList<String> input) {
+        Stack<Integer> stack = new Stack<Integer>();
+		// Question to interviewer : what should we return if the input string is an empty string?
+		if(input == null || input.size() == 0) {
+			 return -1;
+		}
+		for(int i=0;i<input.size();i++) {
+            String current = input.get(i);
+            if(current.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if(current.equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b-a);
+            } else if(current.equals("*")) {
+                stack.push(stack.pop()*stack.pop());
+            } else if(current.equals("/")){
+                int a  =stack.pop();
+                int b = stack.pop();
+                stack.push(b/a);
+            } else {
+                stack.push(Integer.parseInt(current));
+            }
+        }
+        return stack.peek();
+    }
+    
+    public static ArrayList<String> convertInfixToPostfix(String input) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		LinkedList<Character> stack = new LinkedList<Character>();
+		ArrayList<String> result = new ArrayList<String>();
+		map.put('+', 1);
+		map.put('-', 1);
+		map.put('*', 2);
+		map.put('/', 2);
+		int num = 0;
+		for (int i = 0; i < input.length(); i++) {
+			char current = input.charAt(i);
+			if (Character.isDigit(current)) {
+			    num = num*10 + (current -'0');
+			}
+			else {
+			    result.add(String.valueOf(num));
+			    num = 0;
+				while (!stack.isEmpty() && map.containsKey(current) && map.containsKey(stack.peek()) && map.get(stack.peek()) >= map.get(current)) {
+					result.add(String.valueOf(stack.pop()));
+				}
+				stack.push(current);
+			}
+		}
+		while(!stack.isEmpty()) {
+			result.add(String.valueOf(stack.pop()));
+		}
+		return result;
+    }
 }
